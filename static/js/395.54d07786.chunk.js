170e24b53e49f18ea6f4612f7e9e0ef3cc125d16
DmytroKarpenko88 <88401783+DmytroKarpenko88@users.noreply.github.com>
2023-09-21 09:10:06 +0000

    Deploying to gh-pages from @ DmytroKarpenko88/my-car-rental-v2@0b79a56cbd34edd5942cae03da19f975d6af9b94 🚀

---
+++ static/js/395.54d07786.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkmy_car_rental_v2=self.webpackChunkmy_car_rental_v2||[]).push([[395],{395:function(e,r,a){a.r(r);var t=a(510),n=(a(791),a(184));r.default=function(e){var r=e.favorites,a=e.toggleFavorite;return(0,n.jsx)("div",{children:(0,n.jsx)(t.Hg,{cars:r,favorites:r,toggleFavorite:a})})}}}]);
+//# sourceMappingURL=395.54d07786.chunk.js.map
